00a07d25c8f12258e51f12be90dbed83612b0064
Sofiia <matsyhin@Sofiias-MacBook-Air.local>
2020-06-10 00:25:31 +0300

    delete book

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-function Book(title, author, description) {
-    this.title = title;
-    this.author = author;
-    this.description = description;
+class Book {
+    constructor(title, author, description) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+    }
+}
+class UI {
+    addBookToList(book) {
+        const list = document.querySelector('#book-list');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.description}</td>
+        <td><a href="#" class="delete">X</a></td>`;
+        list.appendChild(row);
+    }
+    showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        },3000); 
+    }
+    deleteBook(target) {
+        if(target.className === 'delete') {
+            target.parentElement.parentElement.remove();
+        }  
+    }
+    clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#description').value = '';
+    }
+
 }
 
-function UI() {}
-
-UI.prototype.addBookToList = function(book) {
-    const list = document.querySelector('#book-list');
-    const row = document.createElement('tr');
-    row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.description}</td>
-    <td><a href="#" class="delete">X</a></td>`;
-    list.appendChild(row);
-}
-UI.prototype.showAlert = function(message, className) {
-    const div = document.createElement('div');
-    div.className = `alert ${className}`;
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector('.container');
-    const form = document.querySelector('#book-form');
-    container.insertBefore(div, form);
-    setTimeout(function() {
-        document.querySelector('.alert').remove();
-    },3000);
-}
-UI.prototype.clearFields = function() {
-    document.querySelector('#title').value = '';
-    document.querySelector('#author').value = '';
-    document.querySelector('#description').value = '';
-}
 document.querySelector('#book-form').addEventListener('submit', function(e) {
     const title = document.querySelector('#title').value,
           author = document.querySelector('#author').value,
@@ -48,3 +56,10 @@ document.querySelector('#book-form').addEventListener('submit', function(e) {
     }
     e.preventDefault();
 });
+
+document.querySelector('#book-list').addEventListener('click', function(e) {
+    const ui = new UI();
+    ui.deleteBook(e.target);
+    ui.showAlert('Book Removed!', 'success');
+    e.preventDefault;
+})
